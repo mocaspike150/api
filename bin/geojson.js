d73f13755c3073c046d004151e36cec5e17faeaf
Sam Liu <ontouchstart@gmail.com>
2019-04-17 17:40:54 -0400

    https://www.mocaspike150.org/api/map/club/72363.geojson

---
+++ bin/geojson.js
@@ -0,0 +1,36 @@
+const axios = require('axios')
+const fs = require('fs')
+const api = 'https://www.mocaspike150.org/api'
+
+const feature = (d) => ({
+  "type": "Feature",
+  "geometry": {
+    "type": "Point",
+    "coordinates": [
+      d[0],
+      d[1]
+    ]
+  }
+})
+
+const build = (path) => {
+
+  axios.get(`${api}/${path}`)
+  .then( (res) => {
+    let data = res.data
+    for( k in data) {
+      let geojson = {
+        "type": "FeatureCollection",
+        "features": data[k].map(feature) 
+      }
+      let output = JSON.stringify(geojson)
+      console.log(output)
+      fs.writeFile(`map/club/${k}.geojson`, output, () => {})
+    }
+  })
+  .catch((error) => {
+    console.log(error)
+  });
+}
+
+build('map/club/geo.json')
