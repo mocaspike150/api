aa8557b22e6e923f20d1bdf50e7434120af94a23
Sam Liu <ontouchstart@gmail.com>
2019-04-22 17:17:05 -0400

    leaderboards for the first 5 club

--- bin/leaderboard.js
+++ bin/leaderboard.js
@@ -1,13 +1,20 @@
+const axios = require('axios');
 const fs = require('fs');
+
 const puppeteer = require('puppeteer');
+
 
 const leaderboard = (id) => {
   let url = `https://www.strava.com/clubs/${id}`;
   let text_output = `leaderboard/${id}.text`;
   let html_output = `leaderboard/${id}.html`;
+  console.log(`leaderboard(${id})`);
+
   (async () => {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
+    page.setDefaultTimeout(60000)
+
     await page.goto(url, {waitUntil: 'networkidle2'})
     let text = await page.evaluate(() => document.body.querySelector('.leaderboard').innerText)
     let html = await page.evaluate(() => document.body.querySelector('.leaderboard').innerHTML)
@@ -17,5 +24,21 @@ const leaderboard = (id) => {
  })();
 };
 
-leaderboard('241951');
-leaderboard('327007');
+
+axios.get('https://www.mocaspike150.org/api/club/profile.json')
+  .then( (res) => {
+    let data = res.data
+    let keys = Object.keys(data).slice(0, 5);
+    console.log(data)
+    let index = '<ol>';
+    for( let id of keys) {
+      index += `
+        <li><a href="${id}.html">Leaderboard for ${data[id].en}</a></li>
+        `;
+      leaderboard(id);
+    }
+    index += '</ol>';
+    fs.writeFile('leaderboard/index.html', index, (error) => { if(!error) { console.log(index) } else { console.log(error) } });
+  })
+  .catch((error) => {
+  });
