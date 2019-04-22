d64014810727fc26681a37c031eb4b03b17309fa
Sam Liu <ontouchstart@gmail.com>
2019-04-22 13:45:48 -0400

    leaderboard in html and text

--- bin/leaderboard.js
+++ bin/leaderboard.js
@@ -1,14 +1,21 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
-const id = '327007';
-const url = `https://www.strava.com/clubs/${id}`;
-const output = `leaderboard/${id}.html`;
-(async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto(url, {waitUntil: 'networkidle2'})
-  let leaderboard = await page.evaluate(() => document.body.querySelector('.leaderboard').outerHTML)
-  console.log(leaderboard)
-  fs.writeFile(output, leaderboard, console.log)
+
+const leaderboard = (id) => {
+  let url = `https://www.strava.com/clubs/${id}`;
+  let text_output = `leaderboard/${id}.text`;
+  let html_output = `leaderboard/${id}.html`;
+  (async () => {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto(url, {waitUntil: 'networkidle2'})
+    let text = await page.evaluate(() => document.body.querySelector('.leaderboard').innerText)
+    let html = await page.evaluate(() => document.body.querySelector('.leaderboard').innerHTML)
+    fs.writeFile(text_output, text, (error) => { if(!error) { console.log(text_output) } else { console.log(error) } })
+    fs.writeFile(html_output, html, (error) => { if(!error) { console.log(html_output) } else { console.log(error) } })
   await browser.close()
-})();
+ })();
+};
+
+leaderboard('241951');
+leaderboard('327007');
