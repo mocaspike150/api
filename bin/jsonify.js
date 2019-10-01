95ceb401bb7ac52cbc3d85c8e74c21f201671376
Sam Liu <ontouchstart@gmail.com>
2019-10-01 10:07:50 -0400

    bin/jsonify.js: JSON.stringify(doc)

--- bin/jsonify.js
+++ bin/jsonify.js
@@ -3,7 +3,7 @@ const fs  = require('fs')
 const input = process.argv[2]; 
 try {
   const doc = yaml.safeLoad(fs.readFileSync(input, 'utf8'))
-  console.log(doc)
+  console.log(JSON.stringify(doc))
 } catch (e) {
   console.log(e)
 }
